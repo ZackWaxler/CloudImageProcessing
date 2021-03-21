5d302adcd76a8c18b54d38b4bd164462b22f9002
Zack <tuj00149@temple.edu>
2021-03-20 20:03:46 -0400

    Base app finished, removed incorrectly implemented s3 integration

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from flask import render_template, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
 from datetime import datetime
 
-import boto3
+from PIL import Image, ImageOps, ImageFilter
 
+import boto3
 import logging
 from botocore.exceptions import ClientError
 
@@ -17,11 +18,9 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.secret_key = '123456'
 
 def upload_file(file_name, bucket, object_name=None):
-	# If S3 object_name was not specified, use file_name
 	if object_name is None:
 		object_name = file_name
 
-	# Upload the file
 	s3_client = boto3.client('s3')
 	try:
 		response = s3_client.upload_file(file_name, bucket, object_name)
@@ -30,13 +29,12 @@ def upload_file(file_name, bucket, object_name=None):
 		return False
 	return True
 
-
 def allowed_file(filename):
   return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
 def index():
-  return render_template('index.jinja.html', msg=request.args.get('msg'))
+  return render_template('index.jinja.html')
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
@@ -46,16 +44,44 @@ def upload():
 		file = request.files['image']
 
 		if file and allowed_file(file.filename):
+			filters = []
+
+			form = request.form.to_dict()
+
+			if 'gray' in form: filters.append('gray') 
+			if 'blur' in form: filters.append('blur') 
+			if 'solar' in form: filters.append('solar')
+
+			# Save image to temp folder with unique name
 			uid = datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
 			filename = secure_filename(file.filename)
 			new_name = filename.split('.')[0] + '_' + uid + '.' + filename.split('.')[1]
-			file.save(os.path.join(app.config['UPLOAD_FOLDER'], new_name))
+			path = os.path.join(app.config['UPLOAD_FOLDER'], new_name)
+			file.save(path)
 
-			upload_file(os.path.join(app.config['UPLOAD_FOLDER'], new_name), 'cloudimageprocessing-zw', object_name=new_name)
+			# for each filter
+			# - Apply it & save to tmp
+			for filter_type in filters:
+				img = Image.open(path)
 
-			return redirect(url_for('uploaded', filename=filename))
-		return redirect(url_for('error', msg='Error with file.'))
-	return redirect(url_for('error', msg='Cannot GET /upload'))
+				if filter_type is 'gray':
+					img = ImageOps.grayscale(img)	
+				
+				if filter_type is 'blur':
+					img = img.filter(ImageFilter.BLUR)		
+
+				if filter_type is 'solar':
+					img = ImageOps.solarize(img, threshold=80) 
+
+				img.save(path.replace(uid, uid + '_' + filter_type))
+				# - Upload it
+				# - Redir user to links
+
+			# upload_file(os.path.join(app.config['UPLOAD_FOLDER'], new_name), 'cloudimageprocessing-zw', object_name=new_name)
+
+			return redirect(url_for('uploaded'))
+		return redirect(url_for('error')) # Error with file
+	return redirect(url_for('error')) # GET request
 
 @app.route('/uploaded')
 def uploaded():
@@ -63,7 +89,7 @@ def uploaded():
 
 @app.route('/error')
 def error():
-	return 'working'
+	return 'An error occurred. Redirecting.<script>setTimeout(() => { window.location = "/"; }, 1000)</script>'
 
 # Disable caching static resources
 # Snippet from https://stackoverflow.com/questions/34066804/disabling-caching-in-flask
--- static/main.js
+++ static/main.js
@@ -1,104 +1,124 @@
-$(function() {
-	var clouds = [];
+$(() => {
+	let clouds = [];
 
-	var canvas = document.getElementById('overlayCanvas');
+	let canvas = document.getElementById('overlayCanvas');
 	canvas.width = window.innerWidth;
 	canvas.height = window.innerHeight;
 
+	const anyBoxesChecked = () => {
+		if ($(':checked').length !== 0) {
+			return true;
+		}
+
+		return false;
+	}
+
 	$('#overlay-error').hide();
 
-	$(window).resize(function(e) {
+	$(window).resize((e) => {
 		canvas.width = window.innerWidth;
 		canvas.style.width = window.innerWidth;
 		canvas.height = window.innerHeight;
 		canvas.style.height = window.innerHeight;
 
 		addAllClouds(clouds, 10);
-		console.log('resized')
 	});
 
 	// Display error message when no file is selected.
-	$('form').submit(function(e) {
-		if (!$("form input[type=file]").val()) {
+	$('form').submit((e) => {
+		if (!$("form input[type=file]").val() || !anyBoxesChecked()) {
 			e.preventDefault();
-			var oldText = $('#image-form-label').text();
-			$('#image-form-label').text('Error: No file chosen.');
-			$('#overlay-error').fadeIn(1000, function() {
-				$('#overlay-error').fadeOut(600, function() {
-					$('#image-form-label').text(oldText);
+
+			let oldImageFormLabel = $('#image-form-label').text();
+			let oldFilterFormLabel = $('#filter-form-label').text();
+
+			if (!$("form input[type=file]").val()) { 
+				$('#image-form-label').text('Error: No file chosen.');
+			}
+
+			if (!anyBoxesChecked()) {
+				$('#filter-form-label').text('Error: No filters chosen.');
+			}
+
+			$('#overlay-error').fadeIn(750, () => {
+				$('#overlay-error').fadeOut(500, () => {
+					$('#image-form-label').text(oldImageFormLabel);
+					$('#filter-form-label').text(oldFilterFormLabel);
 				});
 			});
-		}
+		} 
 	});
 
-	var ctx = canvas.getContext('2d');
+	let ctx = canvas.getContext('2d');
 
-	var cloudImage = new Image();
+	let cloudImage = new Image();
 	cloudImage.src = 'static/cloud.png';
 
-	var Cloud = function() {
-		var imageRatio = 1280 / 654;
+	class Cloud {
+		constructor() {
+			const imageRatio = 1280 / 654;
 
-		this.size = 100 + Math.random() * 75
-		this.width = imageRatio * this.size;
-		this.height = this.size;
+			this.size = 100 + Math.random() * 75
+			this.width = imageRatio * this.size;
+			this.height = this.size;
 
-		this.dir = (Math.random() > 0.5 ? 1 : -1);
-		this.x = (this.dir === -1 ? canvas.width : -this.width)
-		this.y = Math.random() * canvas.height;
-		this.velocity = this.dir * (Math.random() + 1);
-	}
+			this.dir = (Math.random() > 0.5 ? 1 : -1);
+			this.x = (this.dir === -1 ? canvas.width : -this.width)
+			this.y = Math.random() * canvas.height;
+			this.velocity = this.dir * (Math.random() + 1);
+		}
 
-	Cloud.prototype.update = function(clouds, i) {
-		this.x += this.velocity;
+		update(clouds, i) {
+			this.x += this.velocity;
 
-		if (this.outOfBounds()) {
-			clouds.push(new Cloud());
-			clouds.splice(i, 1);
-		} 
-	}
+			if (this.outOfBounds()) {
+				clouds.push(new Cloud());
+				clouds.splice(i, 1);
+			} 
+		}
 
-	Cloud.prototype.draw = function(ctx) {
-		ctx.drawImage(cloudImage, 0, 0, 1280, 654, this.x, this.y, this.width, this.height);
-	}
+		draw(ctx) {
+			ctx.drawImage(cloudImage, 0, 0, 1280, 654, this.x, this.y, this.width, this.height);
+		}
 
-	Cloud.prototype.outOfBounds = function() {
-		if (this.x + this.width < 0) {
-			return true;
-		} else if (this.x > canvas.width) {
-			return true;
-		} else {
-			return false;
+		outOfBounds() {
+			if (this.x + this.width < 0) {
+				return true;
+			} else if (this.x > canvas.width) {
+				return true;
+			} else {
+				return false;
+			}			
 		}
 	}
 
-	var numClouds = 3;
-	var FPS = 20;
+	const numClouds = 3;
+	const FPS = 20;
 
-	var addClouds = function(num, time, clouds) {
+	const addClouds = (num, time, clouds) => {
 		setTimeout(function() {
-			for (var i = 0; i < num; i++) {
+			for (let i = 0; i < num; i++) {
 				clouds.push(new Cloud());
 			}			
 		}, time);
 	}
 
-	var addAllClouds = function(clouds, numBursts) {
+	const addAllClouds = (clouds, numBursts) => {
 		if (clouds.length > 0) {
 			clouds = [];
 		}
 
-		for (var i = 0; i < numBursts; i++) {
+		for (let i = 0; i < numBursts; i++) {
 			addClouds(numClouds, 3500 * i, clouds);		
 		}
 	}
 
 	addAllClouds(clouds, 10);
 
-	var animationLoop = setInterval(function() {
+	let animationLoop = setInterval(() => {
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-		for (var i = 0; i < clouds.length; i++) {
+		for (let i = 0; i < clouds.length; i++) {
 			clouds[i].update(clouds, i);
 			clouds[i].draw(ctx);
 		}
